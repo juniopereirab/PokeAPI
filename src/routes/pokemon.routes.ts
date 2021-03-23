a879560e0c44e8ac6470e5c55b2e3d17c33a26c7
Dâmaso Júnio <junio.pereirab@gmail.com>
2021-03-23 00:07:53 -0300

    Adding creating and list functions

--- src/routes/pokemon.routes.ts
+++ src/routes/pokemon.routes.ts
@@ -4,6 +4,8 @@ import pokemonService from '../services/pokemon.service';
 const router = Router();
 
 router.get('/', pokemonService.getWelcomeMessage);
+router.get('/pokemons', pokemonService.listPokemon);
+router.post('/pokemon', pokemonService.createPokemon);
 
 
 export {router};
--- src/services/pokemon.service.ts
+++ src/services/pokemon.service.ts
@@ -1,7 +1,36 @@
 import {Request, Response} from 'express';
 
+import { Pokemon } from '../schema/Pokemon';
+
 export default {
     async getWelcomeMessage(req: Request, res: Response): Promise<Response> {
         return res.send("Welcome to PokeAPI - Damaso");
+    },
+    async createPokemon(req: Request, res: Response): Promise <Response>{
+        const {name, gender, type, height, weight, photo} = req.body;
+        
+        try {
+            const newPokemon = await Pokemon.create({
+                name,
+                gender,
+                type,
+                height,
+                weight,
+                photo
+            });
+    
+            return res.status(201).json(newPokemon);
+        }
+        catch (err) {
+            return res.status(400).json({message: "Error creating Pokemon"});
+        }
+    },
+    async listPokemon(req: Request, res: Response): Promise <Response> {
+        try{
+            const pokemons = await Pokemon.find();
+            return res.status(200).json(pokemons);
+        } catch (err) {
+            return res.status(400).json({message: "Error while listing pokemons"});
+        }
     }
 }
